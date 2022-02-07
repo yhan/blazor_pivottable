6c839eab25e661c2e944cd08c6d54b150c68054f
yhan <hanyi8110@gmail.com>
2022-02-07 14:57:54 +0100

    Repro invalidOp collection modified

--- Pages/PollingService.cs
+++ Pages/PollingService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Threading;
+using Microsoft.AspNetCore.Connections.Features;
 using Syncfusion.Blazor.Internal;
 
 namespace MyBlazorServerApp.Pages
@@ -11,12 +12,15 @@ namespace MyBlazorServerApp.Pages
     {
         private Timer _timer;
         public ObservableCollection<ProductDetails> Observable { get; set; }
-        private int _maxId = 0;
         private readonly object _syncRoot = new object();
-        private const int Size = 200_000;
-        private const int TimerIntervalSecond = 10;
+        private const int InitSize = 200_000;
+        private const int TimerIntervalSecond = 5;
 
         private static PollingService _instance;
+
+        private readonly Random _rand = new Random();
+        private readonly Dictionary<int, Action> _handlers = new Dictionary<int, Action>();
+        private bool _paused;
 
 
         public static PollingService Instance {
@@ -24,48 +28,40 @@ namespace MyBlazorServerApp.Pages
             {
                 if (_instance == null)
                 {
-                    _instance = new PollingService(new ProductDetails[Size]);
+                    _instance = new PollingService();
                 }
 
                 return _instance;
             }
         }
 
-        private readonly Dictionary<int, Action> _handlers = new Dictionary<int, Action>();
-        private bool _paused;
 
-
-        public PollingService(ProductDetails[] arr)
+        public PollingService()
         {
-            Observable = new ObservableCollection<ProductDetails>(arr);
+            //var arr = new ProductDetails[InitSize];
+            Observable = new ObservableCollection<ProductDetails>(/*arr*/);
             _timer = new Timer(Refresh, null, TimeSpan.Zero, TimeSpan.FromSeconds(TimerIntervalSecond));
-            //Refresh(null);
         }
 
         private void Refresh(object? state)
         {
+            if (_paused)
+                return;
+
+            var count = _rand.Next((int)(InitSize * 0.8), (int)(InitSize * 1.2));
+            var arr = new ProductDetails[count];
+            for (var j = 0; j < count; j++)
+            {
+                arr[j] = ProductDetails.BuildOne(j);
+            }
+            
             lock (_syncRoot)
             {
-                if (_paused)
-                    return;
-
-                for (var i = 0; i < Observable.Count; i++)
+                Observable.Clear();
+                foreach (var a in arr)
                 {
-                    if (Observable[i] == null)
-                    {
-                        Observable[i] = ProductDetails.BuildOne(i);
-                    }
-                    else
-                    {
-                        Observable[i].Random();
-                    }
+                    Observable.Add(a);
                 }
-
-                _maxId = Observable.Count - 1;
-
-                Observable.RemoveAt(0);
-                Observable.Add(ProductDetails.BuildOne(++_maxId));
-
                 //Debug.WriteLine($"handlers count = {_handlers.Count}");
                 foreach (var handler in _handlers.Values)
                 {
