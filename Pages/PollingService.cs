ef215974a01989fa96db43cd84ea7087bcbb5c3f
yhan <hanyi8110@gmail.com>
2022-02-07 14:33:31 +0100

    Thread safe add/remove from observable collection

--- Pages/PollingService.cs
+++ Pages/PollingService.cs
@@ -9,21 +9,27 @@ namespace MyBlazorServerApp.Pages
 {
     public class PollingService
     {
-        private Timer timer;
+        private Timer _timer;
         public ObservableCollection<ProductDetails> Observable { get; set; }
+        private int _maxId = 0;
+        private readonly object _syncRoot = new object();
+        private const int Size = 200_000;
+        private const int TimerIntervalSecond = 10;
 
         private static PollingService _instance;
+
+
         public static PollingService Instance {
             get
             {
                 if (_instance == null)
                 {
-                    _instance = new PollingService(new ProductDetails[200_000]);
+                    _instance = new PollingService(new ProductDetails[Size]);
                 }
 
                 return _instance;
             }
-        } 
+        }
 
         private readonly Dictionary<int, Action> _handlers = new Dictionary<int, Action>();
         private bool _paused;
@@ -32,31 +38,39 @@ namespace MyBlazorServerApp.Pages
         public PollingService(ProductDetails[] arr)
         {
             Observable = new ObservableCollection<ProductDetails>(arr);
-            timer = new Timer(Refresh, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+            _timer = new Timer(Refresh, null, TimeSpan.Zero, TimeSpan.FromSeconds(TimerIntervalSecond));
             //Refresh(null);
         }
 
         private void Refresh(object? state)
         {
-            if(_paused)
-                return;
-            
-            for (var i = 0; i < Observable.Count; i++)
+            lock (_syncRoot)
             {
-                if (Observable[i] == null)
-                {
-                    Observable[i] = ProductDetails.BuildOne(i);
-                }
-                else
-                {
-                    Observable[i].Random();
-                }
-            }
+                if (_paused)
+                    return;
 
-            //Debug.WriteLine($"handlers count = {_handlers.Count}");
-            foreach (var handler in _handlers.Values)
-            {
-                handler();
+                for (var i = 0; i < Observable.Count; i++)
+                {
+                    if (Observable[i] == null)
+                    {
+                        Observable[i] = ProductDetails.BuildOne(i);
+                    }
+                    else
+                    {
+                        Observable[i].Random();
+                    }
+                }
+
+                _maxId = Observable.Count - 1;
+
+                Observable.RemoveAt(0);
+                Observable.Add(ProductDetails.BuildOne(++_maxId));
+
+                //Debug.WriteLine($"handlers count = {_handlers.Count}");
+                foreach (var handler in _handlers.Values)
+                {
+                    handler();
+                }
             }
         }
 
